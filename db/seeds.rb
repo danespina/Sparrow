d7a7730c17b21e1970e358e5dafb73349518c844
Dan <danespina0@gmail.com>
2018-09-13 18:47:29 -0400

    Add seed for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+users = User.create({username: "Demo", email: "Demo", password: "123456"})
+
 assets = Asset.create({symbol: "DIS", open: 109.25, close: 109.60})
