992d51b610fdd8713ebeb198d274db9a1dba11b0
Dan <danespina0@gmail.com>
2018-09-17 08:23:47 -0400

    Update trades controller to modify portfolio

--- app/controllers/api/trades_controller.rb
+++ app/controllers/api/trades_controller.rb
@@ -3,22 +3,21 @@ class Api::TradesController < ApplicationController
     @trade = Trade.create(trade_params)
     if @trade.save
       cur_portfolio = Portfolio.find(trade_params[:user_id])
-      if cur_portfolio.holdings[:asset_id]
-        cur_record = cur_portfolio.holdings[:asset_id]
-        cur_portfolio.holdings[:asset_id] = {
+      if cur_portfolio.holdings[trade_params[:asset_id].to_s]
+        cur_record = cur_portfolio.holdings[trade_params[:asset_id].to_s]
+        cur_portfolio.holdings[trade_params[:asset_id].to_s] = {
           asset_id: trade_params[:asset_id],
-          position: trade_params[:position] + cur_record[:position],
-          avg_price: (trade_params[:position] * trade_params[:avg_price]
-            + cur_record[:position] * cur_record[:avg_price])
+          position: trade_params[:position] + cur_record["position"],
+          avg_price: (trade_params[:position] * trade_params[:avg_price].to_f
+            + cur_record["position"] * cur_record["avg_price"].to_f) / (trade_params[:position] + cur_record["position"])
         }
       else
-        cur_portfolio.holdings[:asset_id] = {
+        cur_portfolio.holdings[trade_params[:asset_id].to_s] = {
           asset_id: trade_params[:asset_id],
           position: trade_params[:position],
           avg_price: trade_params[:avg_price]
         }
       end
-      cur_portfolio.merge!
       cur_portfolio.save
       render json: {}
     else
