9ad4dac78cb7cc59615d02742c0f30fb31ee3482
Dan <danespina0@gmail.com>
2018-09-17 10:06:49 -0400

    Add buying power condition to trade

--- app/controllers/api/trades_controller.rb
+++ app/controllers/api/trades_controller.rb
@@ -1,23 +1,30 @@
 class Api::TradesController < ApplicationController
   def create
     @trade = Trade.create(trade_params)
+    trade_cost = trade_params[:position].to_f * trade_params[:avg_price].to_f
+    cur_portfolio = Portfolio.find(trade_params[:user_id])
+    if cur_portfolio.buying_power < trade_cost
+      render json: ["Not enough buying power"], status: 422
+    end
     if @trade.save
-      cur_portfolio = Portfolio.find(trade_params[:user_id])
       if cur_portfolio.holdings[trade_params[:asset_id].to_s]
         cur_record = cur_portfolio.holdings[trade_params[:asset_id].to_s]
+        new_pos = trade_params[:position].to_i + cur_record["position"].to_i
+        old_price = trade_params[:position].to_f * trade_params[:avg_price].to_f
+        new_price = old_price + cur_record["position"].to_f * cur_record["avg_price"].to_f
         cur_portfolio.holdings[trade_params[:asset_id].to_s] = {
-          asset_id: trade_params[:asset_id],
-          position: trade_params[:position] + cur_record["position"],
-          avg_price: (trade_params[:position] * trade_params[:avg_price].to_f
-            + cur_record["position"] * cur_record["avg_price"].to_f) / (trade_params[:position] + cur_record["position"])
+          asset_id: trade_params[:asset_id].to_i,
+          position: new_pos,
+          avg_price: (new_price / new_pos)
         }
       else
         cur_portfolio.holdings[trade_params[:asset_id].to_s] = {
-          asset_id: trade_params[:asset_id],
-          position: trade_params[:position],
-          avg_price: trade_params[:avg_price]
+          asset_id: trade_params[:asset_id].to_i,
+          position: trade_params[:position].to_i,
+          avg_price: trade_params[:avg_price].to_f
         }
       end
+      cur_portfolio.buying_power -= trade_cost
       cur_portfolio.save
       render json: {}
     else
