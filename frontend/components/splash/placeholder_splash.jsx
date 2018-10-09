9f714085aa7f65a3f11037ad6a7d41111122963d
Dan <danespina0@gmail.com>
2018-10-09 07:46:51 -0400

    Update homepage

--- frontend/components/splash/placeholder_splash.jsx
+++ frontend/components/splash/placeholder_splash.jsx
@@ -16,18 +16,64 @@ class Splash extends React.Component {
       return (
         <div className="splash">
           <div className="flex">
-            <div className="left flex-child">
-              <div className="left-header">
-                <h1>Investing.</h1>
-                <h1>Now for the rest of us.</h1>
+            <div className="home-row">
+              <div className="left flex-child">
+                <div className="left-header">
+                  <h1>Investing.</h1>
+                  <h1>Now for the rest of us.</h1>
+                </div>
+                <div className="left-words">
+                  <h4>Sparrow lets you learn to invest in the stock market for free.</h4>
+                </div>
+                <Link to='/signup'>Sign Up</Link>
               </div>
-              <div className="left-words">
-                <h4>Sparrow lets you learn to invest in the stock market for free.</h4>
+              <div className="right flex-child">
+                <img src={phoneURL}></img>
               </div>
-              <Link to='/signup'>Sign Up</Link>
             </div>
-            <div className="right flex-child">
-              <img src={phoneURL}></img>
+            <div className="home-row">
+              <div className="right flex-child">
+                <div className="left-header">
+                  <h1>Invest for free</h1>
+                </div>
+                <div className="left-words">
+                  <h4>We believe that the financial system should work for the rest of us, not just the wealthy.</h4>
+                  <h4>We’ve cut the fat that makes other brokerages costly,
+                    like manual account management and hundreds of storefront locations,
+                    so we can offer zero commission trading.</h4>
+                </div>
+              </div>
+              <div className="left flex-child">
+                <img src={phoneURL}></img>
+              </div>
+            </div>
+            <div className="home-row">
+              <div className="left flex-child">
+                <div className="left-header">
+                  <h1>No manual needed.</h1>
+                </div>
+                <div className="left-words">
+                  <h4>We’ve designed Sparrow from the ground up for the next generation of newcomers and experts alike.</h4>
+                  <h4>It’s fast, dead simple and just works.</h4>
+                </div>
+              </div>
+              <div className="right flex-child">
+                <img src={phoneURL}></img>
+              </div>
+            </div>
+            <div className="home-row">
+              <div className="right flex-child">
+                <div className="left-header">
+                  <h1>Learn by doing.</h1>
+                </div>
+                <div className="left-words">
+                  <h4>With Robinhood, you can learn to invest in the stock market as you build out your portfolio.</h4>
+                  <h4>Discover new stocks through Collections, track your favorites with a personalized news feed, and more.</h4>
+                </div>
+              </div>
+              <div className="left flex-child">
+                <img src={phoneURL}></img>
+              </div>
             </div>
           </div>
         </div>
